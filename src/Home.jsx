af5abe7e0b10c0ec07689ff97390f21214911c86
Ryan White <ryanjwhite10@gmail.com>
2018-08-26 21:39:22 +0000

    Update state with data from API to render reports

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,99 +1,29 @@
 import React, { Component } from 'react';
 import ReportList from './ReportList.jsx';
+import axios from 'axios';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      reports: [
-        {
-          id: 1,
-          name: "French Beach",
-          longitude: "-123.95",
-          latitude: "48.39",
-          stormglass: [
-        {
-          weekday: "Saturday",
-          swellHeight: 0.05,
-          waveHeight: 0.07
-        },
-        {
-          weekday: "Sunday",
-          swellHeight: 0.15,
-          waveHeight: 0.3
-        },
-        {
-          weekday: "Monday",
-          swellHeight: 0.1,
-          waveHeight: 0.15
-        },
-        {
-          weekday: "Tuesday",
-          swellHeight: 0.02,
-          waveHeight: 0.03
-        },
-        {
-          weekday: "Wednesday",
-          swellHeight: 0.03,
-          waveHeight: 0.04
-        },
-        {
-          weekday: "Thursday",
-          swellHeight: 0.05,
-          waveHeight: 0.31
-        }
-        ],
-          created_at: null,
-          updated_at: "2018-08-25T21:45:47.567Z"
-        },
-        {
-          id: 3,
-          name: "China Beach",
-          longitude: "-124.09",
-          latitude: "48.43",
-          stormglass: [
-        {
-          weekday: "Saturday",
-          swellHeight: 1.35,
-          waveHeight: 1.4
-        },
-        {
-          weekday: "Sunday",
-          swellHeight: 1.12,
-          waveHeight: 1.18
-        },
-        {
-          weekday: "Monday",
-          swellHeight: 1.64,
-          waveHeight: 1.67
-        },
-        {
-          weekday: "Tuesday",
-          swellHeight: 1.62,
-          waveHeight: 1.66
-        },
-        {
-          weekday: "Wednesday",
-          swellHeight: 1.46,
-          waveHeight: 1.47
-        },
-        {
-          weekday: "Thursday",
-          swellHeight: 1.18,
-          waveHeight: 1.57
-        }
-        ],
-          created_at: null,
-          updated_at: "2018-08-25T21:45:47.997Z"
-        }
-      ]
+      reports: []
     }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8080/api/beaches')
+      .then(response => {
+        console.log(response);
+        this.setState({ reports: response.data });
+      })
+      .catch(error => console.error(error));
   }
 
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
+        <h2>Surf Forecast</h2>
         <ReportList reports={ this.state.reports } />
       </div>
     )
