46a4d6128a173774d12231caf8f4c6a83c2fb2fb
Cathleen Melendez <cathleen@melendez.com>
2018-08-28 01:45:36 +0000

    Axios post to server and create user and fav beaches

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,22 +1,112 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Register extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      beaches: [],
+      favBeaches: [],
+      currentUserId: null
+    };
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:8080/api/beaches`)
+      .then((res) => {
+        const beaches = res.data;
+        this.setState({ beaches });
+      })
+  };
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    console.log("Target name:", name)
+    this.setState({
+      [name]: value
+    });
+    
+    console.log("STATE:", this.state)
+  }
+
+  addUser(event) {
+    event.preventDefault();
+    const element = event.target.elements;
+
+    const favBeaches = [];
+    const checks = document.getElementsByClassName('checks')
+    for (let i = 0; i < checks.length; i++) {
+      if (checks[i].checked === true) {
+        favBeaches.push(checks[i].name)
+      }
+    }
+    console.log("User's favorite beaches:", favBeaches)
+    
+    console.log(event.target.type === 'checkbox')
+    const user = {first_name: element.first_name.value, 
+                  last_name: element.last_name.value, 
+                  email: element.email.value, 
+                  phone_number: element.phone_number.value, 
+                  password: element.password.value,
+                  favBeaches: favBeaches}
+    console.log("User: ", user)
+
+    axios.post(`http://localhost:8080/register`, user, {
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => {
+      console.log("Response:", res);
+      console.log("res.data:", res.data);
+      if (res.data === "Already registered") {
+        alert("Email already registered!")
+      } else if (res.data === "Fill out all the forms!") {
+        alert("Fill out all the forms!")
+      } else {
+        this.setState({
+          currentUserId: res.data
+        })
+        console.log("STATE:", this.state)
+        element.first_name.value = null;
+        element.last_name.value = null;
+        element.email.value = null;
+        element.phone_number.value = null;
+        element.password.value = null;
+      }
+    })
+    .catch((res) => {
+      console.log("error", res);
+    }); 
+  }
+
   render() {
     return (
       <div>
       <h1>Register</h1>
-      <form action="/register" method="POST">
-        <label for="first_name">First name:</label>
-        <input id="first_name" name="first_name" /><br/>
-        <label for="last_name">Last name:</label>
-        <input id="last_name" name="last_name" /><br/>
-        <label for="email">Email:</label>
-        <input id="email" type="email" name="email" /><br/>
-        <label for="phone">Phone number:</label>
-        <input id="phone" type="tel" name="phone" /><br/>
-        <label for="password">Password:</label>
-        <input id="password" type="password" name="password" /><br/>
-        <input type="submit" value="Submit" />
+      <form onSubmit={this.addUser} action="/register" method="POST">
+        <label htmlFor="first_name">First name:</label>
+        <input id="first_name" name="first_name" onChange={this.handleChange} /><br/>
+        <label htmlFor="last_name">Last name:</label>
+        <input id="last_name" name="last_name" onChange={this.handleChange} /><br/>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" name="email" onChange={this.handleChange} /><br/>
+        <label htmlFor="phone">Phone number:</label>
+        <input id="phone" type="tel" name="phone_number" onChange={this.handleChange} /><br/>
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password" onChange={this.handleChange} /><br/>
+        <br/>
+
+        <label>Pick your favorite beaches:</label>
+            {this.state.beaches.map((beach) => {
+              return <div key={beach.id}><input type="checkbox" className="checks" name={beach.name} onChange={this.handleChange} /><label>{beach.name}</label></div>
+                })}
+          <br/>
+
+          <button type="submit">Register</button>
       </form>
       </div>
     );
