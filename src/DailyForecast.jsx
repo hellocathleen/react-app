6aa22d8c03972512816d9a4b191001b9234c6ed3
Ryan White <ryanjwhite10@gmail.com>
2018-08-30 19:41:14 +0000

    Change date format and add class names for styling

--- src/DailyForecast.jsx
+++ src/DailyForecast.jsx
@@ -6,21 +6,27 @@ import SurfRating from './SurfRating.jsx';
 class DailyForecast extends Component {
   render() {
     const surfData = this.props.surfData;
-    const date = new Date(Number(this.props.timestamp));
-    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const weekday = days[date.getDay()];
-    let rating = [];
+    let date = new Date(Number(this.props.timestamp)).toUTCString();
+    let idx = date.indexOf('201');
+    date = date.slice(0, idx);
 
+    let starRating = [];
     for (let i = 0; i < surfData.surfRating; i++) {
-      rating.push(<SurfRating key={ i }/>);
+      starRating.push(<SurfRating key={ i }/>);
     }
 
     return (
       <div className='daily-forecast'>
-        <h4>{ weekday }</h4>
-        <p><WaveIcon /> { surfData.waveHeight } m &nbsp;&nbsp; { Math.round(surfData.wavePeriod) } s</p>
-        <p><WindIcon /> { Math.round(surfData.windSpeed) } km/h &nbsp; { surfData.windDirection }°</p>
-        <p>{ rating }</p>
+        <h4>{ date }</h4>
+        <div className='wave-div'>
+          <WaveIcon />
+          <span className='wave-data'>{ surfData.waveHeight } m &nbsp;&nbsp; { Math.round(surfData.wavePeriod) } s</span>
+        </div>
+        <div className='wind-div'>
+          <WindIcon />
+          <span className='wind-data'>{ Math.round(surfData.windSpeed) } km/h &nbsp; { surfData.windDirection }°</span>
+        </div>
+        <div className='surf-rating'>{ starRating }</div>
       </div>
     );
   }
