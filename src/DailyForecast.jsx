b8336bcddaf318cee8a6687eb676e07201d6a30f
Ryan White <ryanjwhite10@gmail.com>
2018-08-30 18:52:42 +0000

    Show rating as stars

--- src/DailyForecast.jsx
+++ src/DailyForecast.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import WaveIcon from './WaveIcon.jsx';
 import WindIcon from './WindIcon.jsx';
+import SurfRating from './SurfRating.jsx';
 
 class DailyForecast extends Component {
   render() {
@@ -8,13 +9,18 @@ class DailyForecast extends Component {
     const date = new Date(Number(this.props.timestamp));
     const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const weekday = days[date.getDay()];
+    let rating = [];
+
+    for (let i = 0; i < surfData.surfRating; i++) {
+      rating.push(<SurfRating key={ i }/>);
+    }
 
     return (
       <div className='daily-forecast'>
         <h4>{ weekday }</h4>
         <p><WaveIcon /> { surfData.waveHeight } m &nbsp;&nbsp; { Math.round(surfData.wavePeriod) } s</p>
         <p><WindIcon /> { Math.round(surfData.windSpeed) } km/h &nbsp; { surfData.windDirection }°</p>
-        <p>Rating: { surfData.surfRating } / 5</p>
+        <p>{ rating }</p>
       </div>
     );
   }
