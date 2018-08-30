eaffc02e883bcc2f1c01b5adbca0004ff97a7fdf
Cathleen Melendez <cathleen@melendez.com>
2018-08-30 02:49:24 +0000

    minor modifications

--- src/Login.jsx
+++ src/Login.jsx
@@ -54,6 +54,7 @@ class Login extends Component {
           this.setState(() => ({
             redirectToHome: true
           }))
+          window.location.reload()
         }
       })
       .catch((res) => {
