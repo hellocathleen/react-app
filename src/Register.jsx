5d547db0b6560e8a1d07929fc530025c5725fc7a
Cathleen Melendez <cathleen@melendez.com>
2018-08-31 23:23:34 +0000

    Changed back to .login-register-container

--- src/Register.jsx
+++ src/Register.jsx
@@ -86,7 +86,7 @@ class Register extends Component {
     return (
       <div>
         <img id="background" src="public/images/beach2.png"></img>
-        <div className="register-container">  
+        <div className="login-register-container">  
           <p>Start getting notifications</p>
           <form onSubmit={this.addUser} action="/register" method="POST">
             <label htmlFor="first_name" ></label>
