558ce5a6b69d1b4b035e1ba0e53bfd7ea1e8140a
Ryan White <ryanjwhite10@gmail.com>
2018-08-31 19:04:40 +0000

    Update render and build hourly forecast list

--- src/DailyForecast.jsx
+++ src/DailyForecast.jsx
@@ -1,29 +1,20 @@
 import React, { Component } from 'react';
-import WaveIcon from './WaveIcon.jsx';
-import WindIcon from './WindIcon.jsx';
-import SurfRating from './SurfRating.jsx';
+import HourlyForecast from './HourlyForecast.jsx';
 
 class DailyForecast extends Component {
   render() {
     const { surfData, timestamp } = this.props;
-    const hour = new Date(Number(timestamp)).getHours();
-    const time = hour === 0 ? '12 AM' : hour < 12 ? hour + ' AM' : (hour - 12) + ' PM';
 
-    let starRating = [];
-    for (let i = 0; i < surfData.surfRating; i++) {
-      starRating.push(<SurfRating key={ i }/>);
-    }
+    const hourlyForecast = surfData.map((data, i) => {
+      return <HourlyForecast key={ i } surfData={ data } />
+    });
 
     return (
-      <div className='hourly-forecast'>
-        <h4>{ time }</h4>
-        <WaveIcon />
-        <span className='wave-data'>{ (Math.round(surfData.waveHeight * 10) / 10).toFixed(1) } m </span>
-        <span className='wave-data'>{ Math.round(surfData.wavePeriod) } s </span>
-        <WindIcon />
-        <span className='wind-data'>{ Math.round(surfData.windSpeed) } km/h </span>
-        <span className='wind-data'>{ Math.round(surfData.windDirection) }° </span>
-        <div className='surf-rating'>{ starRating }</div>
+      <div className='daily-forecast'>
+        <h3 className='date'>{ timestamp }</h3>
+        <div className='hourly-forecast'>
+          { hourlyForecast }
+        </div>
       </div>
     );
   }
