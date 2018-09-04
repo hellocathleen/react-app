5447791be261c613a3f95015ac6999563e4218dc
Cathleen Melendez <cathleen@melendez.com>
2018-09-04 21:44:39 +0000

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,43 @@
 var path = require('path');
 var webpack = require('webpack');
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
  
-// const env = dotenv.config().parsed;
+const env = dotenv.config().parsed;
 
-// const envKeys = Object.keys(env).reduce((prev, next) => {
-//   prev[`process.env.${next}`] = JSON.stringify(env[next]);
-//   return prev;
-// }, {});
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
-    entry: [
-      'webpack-dev-server/client?http://localhost:3000',
-      './src/index.jsx'
-    ],
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: 'bundle.js',
-      publicPath: '/build/'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          loader: 'babel-loader',
-          include: path.join(__dirname, 'src')
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-          ]
-        }
-      ]
-    }
-  
+  plugins: [
+    new webpack.DefinePlugin(envKeys)
+  ],
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
 };
-
- 
